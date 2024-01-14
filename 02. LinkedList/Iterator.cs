85bd3c4946c5e13775d12127aa66a8b64ec955b7
YJ <cvcv_cv@naver.com>
2024-01-15 08:36:08 +0900

    feat : Linked List

---
+++ 02. LinkedList/Iterator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._LinkedList
+{
+	public class Iterator
+	{
+		/*******************************************************************
+         * 반복기 (Iterator)
+         * 
+         * 자료구조에 저장되어 있는 요소들을 순차적으로 접근하기 위한 객체
+         * C# 대부분의 자료구조는 반복기를 포함
+         * 이를 통해 자료구조종류와 내부구조에 무관하게 순회가능
+         * 
+         * 처음부터 끝까지(처음부터 끝이 있는 자료들) 순회하고 싶을 때 사용하는 것
+         * IEnumerable<> - foreach
+         * C#의 모든 자료구조에는 IEnumerable이 붙어있다.
+         * 
+         * 강의 다시 듣고 정리하기 조느라 못적음 너무졸림
+         *******************************************************************/
+
+		// <반복기 사용>
+		// 반복기 객체를 자료구조에서 생성하여 순차적으로 확인하며 순회
+		// IEnumerable : 자료구조의 반복기 생성 인터페이스
+		// IEnumerator : 자료구조의 반복기 객체 인터페이스
+
+		static void Main()
+		{
+			List<int> list = new List<int>();
+			LinkedList<int> linkedList = new LinkedList<int>();
+
+			for (int i = 0; i < 10; i++) // 0 ~ 9
+			{
+				list.Add(i);
+				linkedList.AddLast(i);
+			}
+
+			for (int i = 0; i < list.Count; i++)
+			{
+				Console.Write($"{list[i]} ");
+			}
+
+			/*for(int i = 0; i < linkedList.Count; i++) // 잘못된 방법
+			{
+				// Console.Write($"{linkedList[i]"); // error!
+			}*/
+			Console.WriteLine();
+
+			for (LinkedListNode<int> node = linkedList.First; node != null; node = node.Next)
+			{ // 끝이 아닐때까지 반복, 노드의 다음 노드 가르키기 / 노드의 value로 출력하기
+				Console.Write($"{node.Value} ");
+			}
+
+			// 여기서 문제 : 우리가 모르는 구조에는 어떻게 설정할지 고민이다!
+			// 처음부터 끝까지 반복하고 싶다! -> 반복기 사용하면 된다.
+			// 구조를 몰라도 반복기를 사용하면 순회가능하다
+
+			foreach (int element in list)
+			{
+				Console.Write(element);
+			}
+			foreach (int element in linkedList)
+			{
+				Console.Write(element);
+			}
+
+			/*Book book = new Book();
+			foreach (string value in book) // IEnumerable 없이는 안됨
+			{
+
+			}*/
+
+			Average(list);
+			Average(linkedList);
+			Average(Func());
+
+		}
+
+		/*public class Book : IEnumerable<string>
+		{
+			public string[] text;
+		}*/
+
+		public static IEnumerable<int> Func()
+		{
+			yield return 0;
+			yield return 1;
+			yield return 2;
+			yield return 3;
+			yield return 4;
+
+		}
+
+		public static float Average(IEnumerable<int> container)
+		{
+			float average = 0;
+			int count = 0;
+			foreach (int element in container)
+			{
+				average += element;
+				count++;
+			}
+			return average / count;
+		}
+
+	}
+}
---
+++ 02. LinkedList/LinkedList.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure
+{
+	public class LinkedList<T>
+	{
+		private LinkedListNode<T> head; // 0
+		private LinkedListNode<T> tail;
+		private int count;
+
+		public LinkedList() // 0
+		{
+			head = null;
+			tail = null;
+			count = 0;
+		}
+
+		public LinkedListNode<T> First { get { return head; } } // 6
+		public LinkedListNode<T> Last { get { return tail; } } // 6
+		public int Count { get { return count; } } // 6
+
+		public LinkedListNode<T> AddFirst(T value) //  1
+		{
+			LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+			AddBefore(head, value);
+			return newNode;
+		}
+
+		public LinkedListNode<T> AddBefore(LinkedListNode<T> node, T value) // 1
+		{
+			LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+			InsertNodeBefore(node, newNode);
+			return newNode;
+		}
+
+		public LinkedListNode<T> AddLast(T value) // 4
+		{
+			LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+			if (head == null)
+			{
+				InsertNodeToEmptyList(newNode);
+			}
+			else
+			{
+				InsertNodeBefore(tail, newNode);
+			}
+
+			return newNode;
+		}
+
+		public LinkedListNode<T> AddAfter(LinkedListNode<T> node, T value) // 4
+		{
+			LinkedListNode<T> newNode = new LinkedListNode<T>(value);
+			InsertNodeAfter(node, newNode);
+			return newNode;
+		}
+
+		public bool Remove(T value) // 5
+		{
+			LinkedListNode<T> findNode = Find(value);
+			if (findNode != null)
+			{
+				RemoveNode(findNode);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		public void Remove(LinkedListNode<T> node) // 5
+		{
+			RemoveNode(node);
+		}
+		public void RemoveFirst() // 5
+		{
+			RemoveNode(head);
+		}
+		public void RemoveLast() // 5
+		{
+			RemoveNode(tail);
+		}
+
+		public LinkedListNode<T> Find(T value) // 5 // 7
+		{
+			for (LinkedListNode<T> node = First; node != null; node = node.Next)
+			{
+				if (value.Equals(node.Value))
+					return node;
+			}
+			return null;
+		}
+		private void InsertNodeBefore(LinkedListNode<T> node, LinkedListNode<T> newNode) // 2
+		{
+			/*newNode.next = node; // 이거는 node 앞에 아무것도 없었을 때만 해당
+			node.prev = newNode;
+			count++;*/
+
+			LinkedListNode<T> prevNode = node.prev;
+
+			// 1. newNode의 prev를 node의 prev로
+			newNode.prev = prevNode;
+
+			// 2. newNode의 next를 node로
+			newNode.next = node;
+
+			// 3. node의 prev의 next를 newNode로
+			// 3. node 앞의 노드의 유무에 따라 // 3
+			if (node == head) // 3 node 앞에 아무것도 없었을 때(or node가 head이면)
+			{   // 3.1 head를 newNode로
+				head = newNode;
+			}
+			else // 3.2 node 앞에 있었을 때, node의 prev의 next를 newNode로
+			{
+				node.prev.next = newNode; // 2
+			}
+
+			// 4. node의 prev를 newNode로
+			node.prev = newNode;
+
+			count++;
+		}
+
+		public void InsertNodeAfter(LinkedListNode<T> node, LinkedListNode<T> newNode)
+		{
+			newNode.prev = node;
+			newNode.next = node.next;
+			if (node == tail)
+			{
+				tail = newNode;
+			}
+			else
+			{
+				node.next.prev = newNode;
+			}
+			node.next = newNode;
+
+			count++;
+		} // 4
+
+		private void InsertNodeToEmptyList(LinkedListNode<T> newNode) // 4
+		{
+			head = newNode;
+			tail = newNode;
+			count++;
+		}
+
+		private void RemoveNode(LinkedListNode<T> node) // 5
+		{
+			if (node == null)
+				throw new ArgumentNullException("node");
+			if (head == node)
+				head = node.next;
+			if (tail == node)
+				tail = node.prev;
+
+			if (node.prev != null)
+				node.prev.next = node.next;
+			if (node.next != null)
+				node.next.prev = node.prev;
+
+			count--;
+
+		}
+	}
+
+	public class LinkedListNode<T> // 0
+	{
+		private T value;
+
+		public LinkedListNode<T> prev; // 어디에 연결되어 있는지 참조값들 갖고 있음
+		public LinkedListNode<T> next;
+
+		public LinkedListNode(T value)
+		{
+			this.value = value;
+			this.prev = null;
+			this.next = null;
+		}
+		public LinkedListNode(T value, LinkedListNode<T> prev, LinkedListNode<T> next)
+		{
+			this.value = value;
+			this.prev = prev;
+			this.next = next;
+		}
+
+		public LinkedListNode<T> Prev { get { return prev; } }
+		public LinkedListNode<T> Next { get { return next; } }
+		public T Value
+		{
+			get { return value; }
+			set { this.value = value; }
+		}
+	}
+}
