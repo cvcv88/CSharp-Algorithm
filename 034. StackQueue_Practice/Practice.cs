8dcf2a0e8a743eba643b63312b2ca74e521147de
YJ <cvcv_cv@naver.com>
2024-01-15 16:47:22 +0900

    feat : 스택,큐 실습

---
+++ 034. StackQueue_Practice/Practice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _034._StackQueue_Practice
+{
+	public class Practice
+	{
+		static void Main(string[] args)
+		{
+			int[] input = new int[] { 4, 4, 12, 10, 2, 10 };
+			int[] result = ProcessJob(input);
+		}
+
+		static int[] ProcessJob(int[] jobList)
+		{
+			Queue<int> queue = new Queue<int>(jobList);
+			int[] temp = new int[] { };
+			int hour = 8;
+			int day = 1;
+			int count = 0;
+			while(queue.Count > 0)
+			{
+				
+			}
+			return jobList;
+		}
+	}
+
+
+
+}
---
+++ 034. StackQueue_Practice/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _034._StackQueue_Practice
+{
+	internal class Program
+	{
+		static void Main1(string[] args)
+		{
+			Console.WriteLine("텍스트를 입력하세요");
+			Console.Write("> ");
+			string text = Console.ReadLine();
+			if (IsOk(text))
+			{
+				Console.WriteLine("괄호가 유효합니다.");
+			}
+			else
+			{
+				Console.WriteLine("괄호가 유효하지 않습니다.");
+			}
+		}
+
+		static bool IsOk(string text)
+		{
+			Stack<char> stack = new Stack<char>();
+			char temp;
+			foreach (char s in text)
+			{
+				if (s == '(' || s == '[' || s == '{')
+				{
+					stack.Push(s);
+				}
+				else
+				{
+					temp = stack.Peek();
+					if (temp == '(' && s == ')') stack.Pop();
+					else if (temp == '{' && s == '}') stack.Pop();
+					else if (temp == '[' && s == ']') stack.Pop();
+				}
+			}
+			if(stack.Count == 0)
+			{
+				return true;
+			}
+			else
+				return false;
+			
+		}
+	}
+}
