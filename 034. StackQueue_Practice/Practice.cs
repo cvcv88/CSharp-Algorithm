e2d4ff9682c05a7670b6f651e8fcdb6aa1607186
YJ <cvcv_cv@naver.com>
2024-01-15 18:00:15 +0900

    feat : 스택큐 실습

--- 034. StackQueue_Practice/Practice.cs
+++ 034. StackQueue_Practice/Practice.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Quic;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,22 +12,46 @@ namespace _034._StackQueue_Practice
 	{
 		static void Main(string[] args)
 		{
-			int[] input = new int[] { 4, 4, 12, 10, 2, 10 };
-			int[] result = ProcessJob(input);
+			int[] result = ProcessJob(new int[] { 4, 4, 12, 10, 2, 10 });
+			foreach(int day in result)
+			{
+				Console.WriteLine(day);
+			}
 		}
 
+		public const int WorkTime = 8;
 		static int[] ProcessJob(int[] jobList)
 		{
 			Queue<int> queue = new Queue<int>(jobList);
-			int[] temp = new int[] { };
-			int hour = 8;
+			int remainTime = 8;
 			int day = 1;
-			int count = 0;
-			while(queue.Count > 0)
+			List<int> days = new List<int>();
+			/*for(int i = 0; i < jobList.Length; i++)
 			{
-				
+				queue.Enqueue(jobList[i]);
+			}*/ // () 괄호안에 배열 이름 넣어줘도 동일함
+
+			while (queue.Count > 0)
+			{
+				int workTime = queue.Dequeue();
+				while (true)
+				{
+					if (workTime <= remainTime)
+					{
+						remainTime -= workTime;
+						// 작업 완료
+						days.Add(day);
+					}
+					else
+					{
+						workTime -= remainTime;
+						// 다음날로
+						day++;
+						remainTime = 8;
+					}
+				}
 			}
-			return jobList;
+			return days.ToArray(); // 리스트를 array로 만들기
 		}
 	}
 
