7a813bb979d7f516d91c7d646f1bd2d4d6ff6471
YJ <cvcv_cv@naver.com>
2024-01-19 17:59:39 +0900

    feat : DesignTechnique 실습

---
+++ 08. DesignTechnique_Practice/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _08._DesignTechnique_Practice
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int temp = int.Parse(Console.ReadLine());
+			List<int> list = new List<int>();
+			string[] tempArray = Console.ReadLine().Split(' ');
+			int answer = 0;
+			foreach (string str in tempArray)
+			{
+				list.Add(int.Parse(str));
+			}
+
+			list.Sort();
+
+			for (int i = 0; i < temp; i++)
+			{
+				for (int j = temp; j > i; j--)
+				{
+					answer += list[i];
+				}
+			}
+			Console.WriteLine(answer);
+		}
+	}
+}
