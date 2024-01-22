a3198f5ba0875430629dbf62c6b511e9576c4443
YJ <cvcv_cv@naver.com>
2024-01-22 17:59:51 +0900

    feat : sort 실습

---
+++ 09. Sort_Practice/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace _09._Sort_Practice
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Random random = new Random();
+			int count = 10;
+
+			List<int> selectionList = new List<int>(count);
+			List<int> bubbleList = new List<int>(count);
+
+			Console.WriteLine("랜덤 데이터 : ");
+			for(int i = 0; i < count; i++)
+			{
+				int rand = random.Next(1, 100);
+
+				Console.Write($"{rand, 3}");
+				selectionList.Add(rand);
+				bubbleList.Add(rand);
+
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("선택 정렬 결과 : ");
+			Sorting.SelectionSort(selectionList, 0, selectionList.Count - 1);
+			foreach (int value in selectionList)
+			{
+				Console.Write($"{value,3}");
+			}
+			Console.WriteLine();
+
+
+			Console.WriteLine("버블 정렬 결과 : ");
+			  Sorting.BubbleSort(bubbleList, 0, bubbleList.Count - 1);
+			foreach (int value in bubbleList)
+			{
+				Console.Write($"{value,3}");
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ 09. Sort_Practice/Sorting.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._Sort_Practice
+{
+	public class Sorting
+	{
+		public static void SelectionSort(IList<int> list, int start, int end)
+		{
+			for(int i = start; i < end; i++)
+			{
+				// 제일 작은거 골라서 맨 앞에
+				int minIndex = i;
+				for(int j = i; j <= end; j++)
+				{
+					if (list[j] < list[minIndex])
+					{
+						minIndex = j;
+					}
+				}
+				int temp = list[i];
+				list[i] = list[minIndex];
+				list[minIndex] = temp;
+			}
+		}
+
+		public static void InsertionSort(IList<int> list, int start, int end)
+		{
+			for(int i = start; i < end; i++)
+			{
+				for(int j = i; j <= 0; j--)
+				{
+					if (list[j] > list[j - 1])
+					{
+						
+						break;
+					}
+				}
+			}
+		}
+
+		public static void BubbleSort(IList<int> list, int start, int end)
+		{
+			for(int i = start; i < end; i++)
+			{
+				for(int j = start; j < end; j++)
+				{
+					if (list[j] > list[j+1])
+					{
+						int temp = list[j];
+						list[j] = list[j+1];
+						list[j+1] = temp;
+					}
+				}
+			}
+		}
+	}
+}
