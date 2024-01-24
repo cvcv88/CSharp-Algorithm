e21467afffa81f337db1775023f57a17c4073034
YJ <cvcv_cv@naver.com>
2024-01-24 17:56:30 +0900

    feat : search

--- 10. Search_Practice/Program.cs
+++ 10. Search_Practice/Program.cs
@@ -6,7 +6,14 @@ namespace _10._Search_Practice
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			// 순차탐색
+			int[] array = { 1, 3, 5, 7, 9, 8, 6, 4, 2, 0 };
+
+			int indexOf = Array.IndexOf(array, 2);
+			int result = Searching.SequentialSearch(array, 2);
+			Console.WriteLine($"순차탐색 결과 위치 : {indexOf}");
+			Console.WriteLine($"구현한 결과 위치 : {result}");
+			Console.WriteLine();
 		}
 	}
 }
---
+++ 10. Search_Practice/Searching.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._Search_Practice
+{
+	public class Searching
+	{
+		// 순차탐색
+		public static int SequentialSearch<T>(IList<T> list, T item) where T : notnull
+		{
+			for(int i = 0; i <list.Count; i++)
+			{
+				if (list[i].Equals(item))
+				{
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		// 이진탐색
+		public static int BinarySearch<T>(IList<T> list, T item) where T : IComparable<T> 
+		{ 
+
+		}
+
+	}
+}
